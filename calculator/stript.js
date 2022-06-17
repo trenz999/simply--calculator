464198725adcf9334a82b35925df77f7bf8797b9
Joseph Essien <100866443+trenz999@users.noreply.github.com>
2022-06-17 05:24:10 +0100

    Add files via upload

---
+++ calculator/stript.js
@@ -0,0 +1,44 @@
+const display = document.querySelector('.display')
+const controlButtons = document.querySelector('.controls').children
+const allSymbols = ['+', '-', 'x', '÷', 'AC', '=', '.']
+
+let firstValue = ''
+let secondValue = ''
+let symbol = ''
+let result = ''
+
+const calculate = () => {
+    firstValue = parseFloat(firstValue)
+    secondValue = parseFloat(secondValue)
+
+    if (symbol === '+') result = firstValue + secondValue
+    if (symbol === '-') result = firstValue - secondValue
+    if (symbol === 'x') result = firstValue * secondValue
+    if (symbol === '÷') result = firstValue / secondValue
+
+
+    display.innerText = result
+    firstValue = result
+    secondValue = ''
+}
+
+for(let button of controlButtons)  {
+    button.addEventListener('click', () =>{
+        const {innerText: btnValue } = button
+        const btnValueIsSymbol = allSymbols.includes(btnValue)  
+
+        if(!secondValue && btnValue === '=') return null
+        if (btnValue === 'AC') {
+            firstValue = secondValue = symbol = ''
+            return display.innerText = ''
+        }
+        if (firstValue && btnValueIsSymbol){
+            secondValue && calculate()
+            
+            symbol = btnValue
+        }
+        else if (!symbol) firstValue += btnValue
+        else if (symbol) secondValue += btnValue
+        if (btnValue !== '=') display.innerText += btnValue
+    })
+}
